d35b5f1ceda673710c20a9f05f03befb8e1cac0c
ndxbxrme <lewis_the_cat@hotmail.com>
2017-06-12 13:24:00 +0100

    deploy

---
+++ init-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git pull
+npm install
+npm install -g grunt-cli
+bower install
+grunt build
+#groupadd -r vsmaintenance && useradd -m -r -g vsmaintenance vsmaintenance
+su vsmaintenance -c ". env.sh
+screen -X -S VSMAINTENANCE quit || true
+screen -S VSMAINTENANCE node --expose-gc server/app.js"
